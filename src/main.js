7e37176e4942c368d9a94c9a5fcb77e6f9d210ba
tengyuning <tyn1210@163.com>
2019-12-21 15:28:49 +0800

    commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import store from './store'
+import 'swiper/dist/css/swiper.css'
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
